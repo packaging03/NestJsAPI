049bedd60dbfd4e1308570213716eb5135121902
mac <mac@macs-MBP-2.lan>
2022-10-10 11:59:40 +0100

    Initial Commit on this Project

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller('tags')
+export class TagController {
+
+    @Get()
+    findAll() {
+        return ['dragons', 'coffee']
+    }
+}
